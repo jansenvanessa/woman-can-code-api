5a3aa4536a874067387c800055130375dd10bc0d
Vanessa de Moraes Jansen <vanessamjansen@gmail.com>
2021-11-20 19:55:17 -0300

    merging with original code

--- meu-medico-favorito-final/src/app.js
+++ meu-medico-favorito-final/src/app.js
@@ -1,49 +1,34 @@
-const express = require("express");
-const app = express();
+const express = require("express")
+const app = express()
+app.use(express.json())
 
 //rotas
-const index = require("./routes/index");
-
-app.use(express.json());
+const index = require("./routes/index")
+const doctors = require("./routes/doctors")
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
-app.use("/", index);
-
-// app.patch("/cors", (req, res) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Content-Type,Content-Length,Server,Date,access-control-allow-methods,access-control-allow-origin"
-//   );
-//   res.header(
-//     "Access-Control-Allow-Methods",
-//     "PUT,POST,GET,DELETE,OPTIONS,PATCH"
-//   );
-//   res.send("ok");
-// });
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Request-With, Content-Type, Accept"
+    )
+    next()
+})
 
 app.options("/*", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers"
-    // "Content-Type,Content-Length,Server,Date,access-control-allow-methods,access-control-allow-origin"
-  );
-  res.header(
-    "Access-Control-Allow-Methods",
-    "PUT,POST,GET,DELETE,OPTIONS,PATCH"
-  );
-  res.send("send some thing whatever");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers");
+    res.header(
+        "Access-Control-Allow-Methods",
+        "PUT,POST,GET,DELETE,OPTIONS,PATCH"
+    );
+    res.send("send some thing whatever");
 });
 
-const doctors = require("./routes/doctors");
-app.use("/doctors", doctors);
+app.use("/", index)
+app.use("/doctors", doctors)
 
-module.exports = app;
+module.exports = {
+    app
+}
+
