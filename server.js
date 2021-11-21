b1064eae6a658b63c61ac8bf08b645acce5282e9
Vanessa de Moraes Jansen <vanessamjansen@gmail.com>
2021-11-20 23:22:12 -0300

    changing API PORT to PORT

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const { app } = require("./src/app")
-const port = process.env.API_PORT || 3000
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
     console.log(`Servidor está rodando na porta ${port}`)
