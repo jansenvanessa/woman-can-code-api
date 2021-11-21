b29b9664ff399eb4651b9cebae18af83cbe0ff06
Vanessa de Moraes Jansen <vanessamjansen@gmail.com>
2021-11-20 23:51:30 -0300

    removing coding

--- server.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config()
-const { app } = require("./src/app")
-const port = process.env.PORT || 3000
-
-app.listen(port, () => {
-    console.log(`Servidor está rodando na porta ${port}`)
-})
