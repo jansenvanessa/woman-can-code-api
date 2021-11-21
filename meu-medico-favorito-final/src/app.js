8e0ed7b3e2f1e9cc0540745ca87d6d00a1ee6aea
Vanessa de Moraes Jansen <vanessamjansen@gmail.com>
2021-11-20 21:21:24 -0300

    adding comments

--- meu-medico-favorito-final/src/app.js
+++ meu-medico-favorito-final/src/app.js
@@ -13,6 +13,8 @@ app.use(function (req, res, next) {
         "Origin, X-Request-With, Content-Type, Accept"
     )
     next()
+    // como criei uma função dentro do app.use, preciso dar um "next()" para mandar ele seguir para a próxima middleware. 
+    // se eu não faço isso, a requisição vai ficar travada aí.
 })
 
 app.options("/*", (req, res) => {
@@ -25,6 +27,7 @@ app.options("/*", (req, res) => {
     res.send("send some thing whatever")
 })
 
+// para que o front consiga utilizar as rotas que vamos criar precisamos dar essa permissão de acesso
 app.use("/", index)
 app.use("/doctors", doctors)
 
