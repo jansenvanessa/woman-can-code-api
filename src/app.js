0c29084e2e7f5ca7826f6e0f69715411dcfe8cdd
Vanessa de Moraes Jansen <vanessamjansen@gmail.com>
2021-11-26 22:49:29 -0300

    adding app options

--- src/app.js
+++ src/app.js
@@ -15,6 +15,17 @@ app.use(function (req, res, next) {
   next();
 });
 
+// para que o front consiga utilizar as rotas que vamos criar precisamos dar essa permissão de acesso
+app.options("/*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers")
+  res.header(
+    "Access-Control-Allow-Methods",
+    "PUT,POST,GET,DELETE,OPTIONS,PATCH"
+  );
+  res.send("send some thing whatever")
+})
+
 app.use("/", index)
 
 app.use("/doctors", doctors)
